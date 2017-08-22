7601672739b570d328690750c295d7fad44ca467
YINNNER <yulianwei1998@163.com>
2017-08-22 19:36:55 +0800

    comment2

--- PlaneGame/Classes/HelloWorldScene.cpp
+++ PlaneGame/Classes/HelloWorldScene.cpp
@@ -66,6 +66,8 @@ bool HelloWorld::init()
 
     // add "HelloWorld" splash screen"
     auto sprite = Sprite::create("res/Warplane.jpg");
+    
+    
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
 
