ed2bda3806efc3516060d25092113c45a2417cc8
gongmm <gnaizgnaw@163.com>
2017-08-24 15:02:34 +0800

    修改

--- WarPlane/Classes/CreateLayer.cpp
+++ WarPlane/Classes/CreateLayer.cpp
@@ -5,7 +5,7 @@ bool CreateLayer :: init() {
 	auto winSize = Director::getInstance()->getWinSize();
 	auto origin = Director::getInstance()->getVisibleOrigin();
 	
-	auto _background = Sprite::create("/res/UI/a1CreatePlayer/background.png");
+	auto _background = Sprite::create("/res/UI/1Menu/background.png");
 	_background->setPosition(Vec2(origin.x + winSize.width /2, origin.y + winSize.height / 2));
 	auto scale = winSize.width / _background->getContentSize().width;
 	_background->setScale(scale);
