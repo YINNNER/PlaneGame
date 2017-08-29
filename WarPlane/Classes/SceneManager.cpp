f1e4ad80228b0e1aae6dc75c452d155a7c1d88ea
Panda98 <810650217@qq.com>
2017-08-29 15:17:26 +0800

    修改help和set界面的跳转

--- WarPlane/Classes/SceneManager.cpp
+++ WarPlane/Classes/SceneManager.cpp
@@ -170,7 +170,7 @@ void SceneManager::goSetLayer(int tag) {
 	SetLayer * layer = SetLayer::create();
 	setScene->addChild(layer);*/
 	auto transition = TransitionMoveInR::create(0.2f, setScene);
-	Director::getInstance()->replaceScene(transition);
+	Director::getInstance()->pushScene(transition);
 	/*	times++;
 	}
 	else {
