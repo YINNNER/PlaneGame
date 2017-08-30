af42a2953fd8b3487618e45a292b751c8df6a2ce
Panda98 <810650217@qq.com>
2017-08-30 08:51:59 +0800

    buglist

--- WarPlane/Classes/Bullet.cpp
+++ WarPlane/Classes/Bullet.cpp
@@ -21,7 +21,7 @@ void Bullet::setBulletImg(const char * filename) {
 	
 }
 void Bullet::update(float dt) {
-	static VISIBLESIZE;
+	auto visibleSize = Director::getInstance()->getVisibleSize();
 	if (type==1)
 	{
 		this->setPositionY(this->getPositionY() + 5);
@@ -49,7 +49,7 @@ void Bullet::removeBullet()
 }
 void Bullet::skillMove(float dt)
 {
-	static VISIBLESIZE;
+	auto visibleSize = Director::getInstance()->getVisibleSize();
 	this->setPositionY(this->getPositionY() + 30);
 	if (this->getPositionY() > visibleSize.height)
 	{
