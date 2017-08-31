69fffaa3e57b683bbabc9a11843d4a6017e6186e
Panda98 <810650217@qq.com>
2017-08-31 15:34:32 +0800

    优化

--- WarPlane/Classes/MapLayer.cpp
+++ WarPlane/Classes/MapLayer.cpp
@@ -17,8 +17,8 @@ bool MapLayer::init() {
 	auto animation = Animation::create();
 	for (int i = 1; i <= 3; i++) {
 		char szName[100] = { 0 };
-		sprintf(szName, "res/UI/a2SelectLevel/%d.png", i);
-		animation->addSpriteFrameWithFile(szName);
+		sprintf(szName, "%d.png", i);
+		animation->addSpriteFrameWithFileName(szName);
 	}
 	animation->setDelayPerUnit(0.1f);
 	animation->setRestoreOriginalFrame(true);
